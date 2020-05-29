f0338229fe899306d0c98b44a770bd3ea26fd3cb
=GustavoTA <gustavotacs7@gmail.com>
2020-05-29 17:51:02 -0300

    Cross Generico

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+    res.setHeader('Access-Control-Allow-Origin', 'all')
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     // Request headers you wish to allow
