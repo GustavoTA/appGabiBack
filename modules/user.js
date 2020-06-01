c4c2b03460fee035fb9cce8b35d6903c7405b882
=GustavoTA <gustavotacs7@gmail.com>
2020-06-01 11:33:01 -0300

    Inclusão da rota user -> Correção Schema 2

--- modules/user.js
+++ modules/user.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userScheme = new Schema({
+const resposta = new Schema({
+    idPergunta: {type: Number},
+    idResposta: {type: Number},
+
+})
+
+const UserSchema = new Schema({
     _id : { type: Number },
     nome : { type: String },
     apelido : { type: String },
@@ -9,10 +15,6 @@ const userScheme = new Schema({
     repostas : resposta
 })
 
-const resposta = new Schema({
-    idPergunta: {type: Number},
-    idResposta: {type: Number},
 
-})
 
 module.exports = mongoose.model('User', UserSchema);
